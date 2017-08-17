2d773904158b0f21b089014deb269ec66004d9bb
bodo92 <30963223+bodo92@users.noreply.github.com>
2017-08-17 19:26:02 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -51,3 +51,5 @@ plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilo
 ## Save file and close device
 dev.copy(png,"plot2.png", width=480, height=480)
 dev.off()
+dev.off()
+dev.off()
